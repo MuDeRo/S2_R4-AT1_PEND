306d7126a4ec354ed65f78170ad6701b02715e42
MURILO DEMORI RODRIGUES <murilo.rodrigues35@senaisp.edu.br>
2026-05-07 12:29:02 -0300

    conexão com banco de dados

---
+++ backend/src/routes/routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+const routes = Router();
+import categoriaRoutes from "./categoriaRoutes.js";
+
+routes.use('/categorias', categoriaRoutes);
+
+export default routes;
