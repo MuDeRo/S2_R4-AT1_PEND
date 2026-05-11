a33fe32d769aa0ac8d7a4be4a881d9b9cffe79eb
MURILO REIS DOTA <murilo.dota2@senaisp.edu.br>
2026-05-11 14:35:32 -0300

    feat: criação da listagem de produtos

---
+++ frontend/src/components/shared/coluna-bootstrap.component.js
@@ -0,0 +1,5 @@
+export default function criarColuna() {
+    const coluna = document.createElement('div');
+    coluna.className = 'col-md-4 mb-4';
+    return coluna;
+}
--- frontend/src/pages/produtos/produtos.page.js
+++ frontend/src/pages/produtos/produtos.page.js
@@ -1,3 +1,25 @@
+import { buscarProdutos } from "../../services/produtos/produtos.api.js";
 import criarCardProduto from "../../components/produtos/card.component.js";
+import criarColuna from "../../components/shared/coluna-bootstrap.component.js";
 
-export {criarCardProduto};
+export async function produtosPage() {
+    const app = document.querySelector("#app");
+
+    app.innerHTML = `
+        <h1 class="fw-bold text-primary">🛒 Produtos</h1>
+        <div class="row mt-4" id="lista-produtos"></div>
+    `;
+
+    const row = document.querySelector("#lista-produtos");
+
+    const produtos = await buscarProdutos();
+
+    produtos.forEach(produto => {
+        const coluna = criarColuna();
+
+        const card = criarCardProduto(produto);
+
+        coluna.appendChild(card);
+        row.appendChild(coluna);
+    });
+}
