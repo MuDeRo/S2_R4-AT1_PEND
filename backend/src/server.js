ecf8d38ed1924df11ba0c951ab9656da451b05e4
MURILO DEMORI RODRIGUES <murilo.rodrigues35@senaisp.edu.br>
2026-05-11 13:42:15 -0300

    update: mudança no server de localhost para uma hospedagem local na rede

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.json()); 
 app.use('/', routes); 
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Servidor rodando em: http://localhost:${process.env.SERVER_PORT}`);
+app.listen(process.env.SERVER_PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando em: http://10.87.169.65:${process.env.SERVER_PORT}`);
 });
