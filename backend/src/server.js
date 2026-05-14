b5505bad874f331f45438afa6bf3113f05828985
MURILO DEMORI RODRIGUES <murilo.rodrigues35@senaisp.edu.br>
2026-05-14 13:24:56 -0300

    update: correção de erros bobos

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 app.use(express.json()); 
-app.use('/', routes); 
+app.use('/', routes);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Servidor rodando em: http://localhost:${process.env.SERVER_PORT}`);
